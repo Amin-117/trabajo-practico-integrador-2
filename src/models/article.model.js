902500e1e562f1165ffbf5fe778033c46221e6e6
Amin_117 <garridoamin@gmail.com>
2025-09-19 15:01:18 -0300

    creacion del modelo para article

---
+++ src/models/article.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const articleSchema = new Schema({
+  tittle: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 200,
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: 500,
+  },
+  excerpt: {
+    type: String,
+    maxlength: 500,
+    required: false,
+  },
+  status: {
+    type: String,
+    enum: ["archived", "published"],
+    default: "published",
+  },
+  Author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  deletedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+export const ArticleModel = model("Article", articleSchema);
